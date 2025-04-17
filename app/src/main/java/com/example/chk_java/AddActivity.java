73badb22028ff9ab1289847fb74de8f5a12cbd24
prayanne <kyhy0321@gmail.com>
2025-04-17 19:25:07 +0900

    0417-Acitvity, LifeCycle, create, stop,pause, restart, destroy

--- app/src/main/java/com/example/chk_java/AddActivity.java
+++ app/src/main/java/com/example/chk_java/AddActivity.java
@@ -2,6 +2,7 @@ package com.example.chk_java;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +15,8 @@ public class AddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
+        Log.d("ActivityTest", "onCreate()");
+
         Button btn1 = (Button) findViewById(R.id.goBackActivity);
 
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +26,29 @@ public class AddActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("ActivityTest", "onStop()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("ActivityTest", "onPause()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("ActivityTest", "onDestroy()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("ActivityTest", "onRestart()");
     }
 }
