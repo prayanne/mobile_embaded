f30e5647c68760b09d6aa6b5fd4a65b8a571ef47
prayanne <kyhy0321@gmail.com>
2025-04-24 19:44:13 +0900

    almost_done

--- app/src/main/java/com/example/gallery/MainActivity.java
+++ app/src/main/java/com/example/gallery/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.gallery;
 
+import android.content.Intent;
+import android.graphics.Picture;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,8 +21,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    void clickDraw(){
+    void clickDraw(View v){
+        int id = v.getId();
+        LinearLayout layout = (LinearLayout) v.findViewById(id);
+        String tag = (String) layout.getTag();
 
+//        Toast.makeText(this, "클릭한 아이템", Toast.LENGTH_SHORT).show();
+        Intent it = new Intent(this, PictureActivity.class);
+        it.putExtra("it_tag", tag);
+        startActivity(it);
     }
 
 }
